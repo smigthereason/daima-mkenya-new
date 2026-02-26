8c9f2d185b0961459335d9c3cb74eebeb8d2aaaf
Victor Maina <148073062+smigthereason@users.noreply.github.com>
2026-02-26 16:08:10 +0300

    Update index.ts

--- public/assets/index.ts
+++ public/assets/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // Branding & Hero Assets
 export { default as HeroImage } from "./daima-mkenya-studio-hero-2026.webp";
 export { default as HeroImage2 } from "./kenyan-heritage-fashion-grayscale.webp";
@@ -52,57 +53,5 @@ export { default as Product13 } from "./hand-stitched-textile-clutch.webp";
 export { default as Product14 } from "./striped-national-button-down.webp";
 export { default as Product15 } from "./leopard-shield-print-vest-3.webp";
 
-// Gallery Subfolder - Explicit naming to avoid any confusion
-import G_Earth1 from "./gallery/1000317885.webp";
-import G_Earth2 from "./gallery/1000317888.webp";
-import G_Earth3 from "./gallery/1000317894.webp";
-import G_Earth4 from "./gallery/1000317924.webp";
-import G_Earth5 from "./gallery/1000317943.webp";
-import G_Earth6 from "./gallery/1000317945.webp";
-
-import G_Urban1 from "./gallery/1000317966.webp";
-import G_Urban2 from "./gallery/1000317968.webp";
-import G_Urban3 from "./gallery/1000317970.webp";
-import G_Urban4 from "./gallery/1000317972.webp";
-import G_Urban5 from "./gallery/1000317974.webp";
-import G_Urban6 from "./gallery/1000317976.webp";
-import G_Urban7 from "./gallery/1000317978.webp";
-import G_Urban8 from "./gallery/1000317982.webp";
-import G_Urban9 from "./gallery/1000317987.webp";
-
-import G_Verdant1 from "./gallery/IMG-20260225-WA0012.webp";
-import G_Verdant2 from "./gallery/IMG-20260225-WA0013.webp";
-import G_Verdant3 from "./gallery/IMG-20260225-WA0014.webp";
-import G_Verdant4 from "./gallery/IMG-20260225-WA0015.webp";
-import G_Verdant5 from "./gallery/IMG-20260225-WA0016.webp";
-import G_Verdant6 from "./gallery/IMG-20260225-WA0017.webp";
-import G_Verdant7 from "./gallery/IMG-20260225-WA0018.webp";
-import G_Verdant8 from "./gallery/IMG-20260225-WA0019.webp";
-import G_Verdant9 from "./gallery/IMG-20260225-WA0020.webp";
-import G_Verdant10 from "./gallery/IMG-20260225-WA0021.webp";
-import G_Verdant11 from "./gallery/IMG-20260225-WA0022.webp";
-import G_Verdant12 from "./gallery/IMG-20260225-WA0023.webp";
-import G_Verdant13 from "./gallery/IMG-20260225-WA0024.webp";
-import G_Verdant14 from "./gallery/IMG-20260225-WA0028.webp";
-import G_Verdant15 from "./gallery/IMG-20260225-WA0029.webp";
-import G_Verdant16 from "./gallery/IMG-20260225-WA0034.webp";
-import G_Verdant17 from "./gallery/IMG-20260225-WA0035.webp";
-import G_Verdant18 from "./gallery/IMG-20260225-WA0037.webp";
-import G_Verdant19 from "./gallery/IMG-20260225-WA0038.webp";
-import G_Verdant20 from "./gallery/IMG-20260225-WA0039.webp";
-import G_Verdant21 from "./gallery/IMG-20260225-WA0040.webp";
-import G_Verdant22 from "./gallery/IMG-20260225-WA0041.webp";
-import G_Verdant23 from "./gallery/IMG-20260225-WA0043.webp";
-import G_Verdant24 from "./gallery/IMG-20260225-WA0044.webp";
-
-import G_Amber1 from "./gallery/IMG-20260225-WA0059.webp";
-import G_Amber2 from "./gallery/IMG-20260225-WA0060.webp";
-import G_Amber3 from "./gallery/IMG-20260225-WA0068.webp";
-import G_Amber4 from "./gallery/IMG-20260225-WA0069.webp";
-
-export {
-  G_Earth1, G_Earth2, G_Earth3, G_Earth4, G_Earth5, G_Earth6,
-  G_Urban1, G_Urban2, G_Urban3, G_Urban4, G_Urban5, G_Urban6, G_Urban7, G_Urban8, G_Urban9,
-  G_Verdant1, G_Verdant2, G_Verdant3, G_Verdant4, G_Verdant5, G_Verdant6, G_Verdant7, G_Verdant8, G_Verdant9, G_Verdant10, G_Verdant11, G_Verdant12, G_Verdant13, G_Verdant14, G_Verdant15, G_Verdant16, G_Verdant17, G_Verdant18, G_Verdant19, G_Verdant20, G_Verdant21, G_Verdant22, G_Verdant23, G_Verdant24,
-  G_Amber1, G_Amber2, G_Amber3, G_Amber4
-};
+// Note: Gallery images are now handled dynamically in GalleryPage.tsx
+// No static gallery imports needed here
