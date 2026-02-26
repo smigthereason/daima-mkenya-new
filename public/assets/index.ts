5ecf35f9706081e526595b50d498e40a90c9e3e1
Victor Maina <148073062+smigthereason@users.noreply.github.com>
2026-02-26 15:36:52 +0300

    Update index.ts

--- public/assets/index.ts
+++ public/assets/index.ts
@@ -22,7 +22,7 @@ import SDG1 from "./shield-stripe-maxi-skirt.webp";
 
 // Collection Model Images
 import Model1 from "./kenyan-striped-poncho-2.webp";
-import Model2 from "./2.webp"; // Kept as 2.webp as per your folder list
+import Model2 from "./2.webp";
 import Model3 from "./runway-poplin-shirt-short-set.webp";
 import Model4 from "./tribal-inspired-modern-outerwear.webp";
 import Model5 from "./unity-ribbed-tank-top.webp";
@@ -51,6 +51,54 @@ import Product12 from "./artisan-made-earrings-gold.webp";
 import Product13 from "./hand-stitched-textile-clutch.webp";
 import Product14 from "./striped-national-button-down.webp";
 import Product15 from "./leopard-shield-print-vest-3.webp";
+
+// Gallery Subfolder Images
+import G_Earth1 from "./gallery/1000317885.webp";
+import G_Earth2 from "./gallery/1000317888.webp";
+import G_Earth3 from "./gallery/1000317894.webp";
+import G_Earth4 from "./gallery/1000317924.webp";
+import G_Earth5 from "./gallery/1000317943.webp";
+import G_Earth6 from "./gallery/1000317945.webp";
+
+import G_Urban1 from "./gallery/1000317966.webp";
+import G_Urban2 from "./gallery/1000317968.webp";
+import G_Urban3 from "./gallery/1000317970.webp";
+import G_Urban4 from "./gallery/1000317972.webp";
+import G_Urban5 from "./gallery/1000317974.webp";
+import G_Urban6 from "./gallery/1000317976.webp";
+import G_Urban7 from "./gallery/1000317978.webp";
+import G_Urban8 from "./gallery/1000317982.webp";
+import G_Urban9 from "./gallery/1000317987.webp";
+
+import G_Verdant1 from "./gallery/IMG-20260225-WA0012.webp";
+import G_Verdant2 from "./gallery/IMG-20260225-WA0013.webp";
+import G_Verdant3 from "./gallery/IMG-20260225-WA0014.webp";
+import G_Verdant4 from "./gallery/IMG-20260225-WA0015.webp";
+import G_Verdant5 from "./gallery/IMG-20260225-WA0016.webp";
+import G_Verdant6 from "./gallery/IMG-20260225-WA0017.webp";
+import G_Verdant7 from "./gallery/IMG-20260225-WA0018.webp";
+import G_Verdant8 from "./gallery/IMG-20260225-WA0019.webp";
+import G_Verdant9 from "./gallery/IMG-20260225-WA0020.webp";
+import G_Verdant10 from "./gallery/IMG-20260225-WA0021.webp";
+import G_Verdant11 from "./gallery/IMG-20260225-WA0022.webp";
+import G_Verdant12 from "./gallery/IMG-20260225-WA0023.webp";
+import G_Verdant13 from "./gallery/IMG-20260225-WA0024.webp";
+import G_Verdant14 from "./gallery/IMG-20260225-WA0028.webp";
+import G_Verdant15 from "./gallery/IMG-20260225-WA0029.webp";
+import G_Verdant16 from "./gallery/IMG-20260225-WA0034.webp";
+import G_Verdant17 from "./gallery/IMG-20260225-WA0035.webp";
+import G_Verdant18 from "./gallery/IMG-20260225-WA0037.webp";
+import G_Verdant19 from "./gallery/IMG-20260225-WA0038.webp";
+import G_Verdant20 from "./gallery/IMG-20260225-WA0039.webp";
+import G_Verdant21 from "./gallery/IMG-20260225-WA0040.webp";
+import G_Verdant22 from "./gallery/IMG-20260225-WA0041.webp";
+import G_Verdant23 from "./gallery/IMG-20260225-WA0043.webp";
+import G_Verdant24 from "./gallery/IMG-20260225-WA0044.webp";
+
+import G_Amber1 from "./gallery/IMG-20260225-WA0059.webp";
+import G_Amber2 from "./gallery/IMG-20260225-WA0060.webp";
+import G_Amber3 from "./gallery/IMG-20260225-WA0068.webp";
+import G_Amber4 from "./gallery/IMG-20260225-WA0069.webp";
 
 export {
   HeroStripe,
@@ -101,4 +149,9 @@ export {
   Product13,
   Product14,
   Product15,
+  // Gallery Exports
+  G_Earth1, G_Earth2, G_Earth3, G_Earth4, G_Earth5, G_Earth6,
+  G_Urban1, G_Urban2, G_Urban3, G_Urban4, G_Urban5, G_Urban6, G_Urban7, G_Urban8, G_Urban9,
+  G_Verdant1, G_Verdant2, G_Verdant3, G_Verdant4, G_Verdant5, G_Verdant6, G_Verdant7, G_Verdant8, G_Verdant9, G_Verdant10, G_Verdant11, G_Verdant12, G_Verdant13, G_Verdant14, G_Verdant15, G_Verdant16, G_Verdant17, G_Verdant18, G_Verdant19, G_Verdant20, G_Verdant21, G_Verdant22, G_Verdant23, G_Verdant24,
+  G_Amber1, G_Amber2, G_Amber3, G_Amber4
 };
